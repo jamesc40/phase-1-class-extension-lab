6ea51b2be55e525d8d728bf82942d5ad604e43f6
James Coffman <jamesc4023@gmail.com>
2022-04-21 17:42:36 -0400

    commit tree 203b70c9c4fa858fc3f29ac277a14dd22c6bb2de

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+class Polygon{
+    constructor(arr){
+        this.arr = arr
+    }
+
+    get countSides(){
+        return this.arr.length;
+    }
+
+    get perimeter(){
+        return this.arr.reduce((sum, side) => sum + side)
+
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        if(this.arr[0] + this.arr[1] > this.arr[2] && this.arr[1] + this.arr[2] > this.arr[0] && this.arr[2] + this.arr[0] > this.arr[1]){
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+class Square extends Polygon{
+    get isValid(){
+        if(this.arr[0] === this.arr[1] && this.arr[1] === this.arr[2] && this.arr[2] === this.arr[3]) return true
+        else return false
+    }
+    get area(){
+        return Math.pow(this.arr[0], 2)
+    }
+}
+
